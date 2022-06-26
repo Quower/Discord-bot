2c352d53ee18de65cba943b4415bc0bbac4bf84f
Quower <quowerhiller@gmail.com>
2022-06-26 10:28:16 +0300

    update stuff

---
+++ commands/play.ts
@@ -0,0 +1,39 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'music commands',
+    description: 'starts playing music',
+
+    slash: true,
+    testOnly: true,
+    options: [ 
+        {
+            name: "song",
+            description: "link or name of the song you want to play",
+            type: 'STRING',
+            required: true
+        },
+        {
+            name: "platform",
+            description: "what platform to seach song on",
+            type: 'STRING',
+            required: false,
+            choices: [{ name: 'spotify', value: 'spotify' }, { name: 'youtube', value: 'youtube' }],
+            
+        }
+    ],
+    
+
+    callback: async ({client, guild, member, interaction}) => {
+        if (!member.voice.channel) {
+            interaction.reply({
+                content: 'you are not in a vc',
+                ephemeral: true
+            })
+        } 
+     
+
+
+
+    },
+} as ICommand
