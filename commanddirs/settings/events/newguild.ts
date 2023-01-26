2236e3a5699b134c324d5bc8980e14bc16aa28c6
Quower odroid <odroid@example.com>
2023-01-26 16:03:42 +0200

    changes

---
+++ commanddirs/settings/events/newguild.ts
@@ -0,0 +1,15 @@
+import optionsSchema from "../../../handler/models/optionsSchema";
+import { myEvent, readyEvent } from "../../../handler/typings";
+import fs from "fs";
+import { saveSetting, settingsCategory } from "../typings";
+const config = require("../../../config.json");
+import { Schema } from "mongoose";
+import { Events } from "discord.js";
+
+
+export default {
+    event: Events.GuildCreate,
+  async execute(guild, client) {
+    
+  },
+} as myEvent;
